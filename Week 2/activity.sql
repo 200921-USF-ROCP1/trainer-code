da117148e73f50842dd6d485d815fde1864c6be8
Jacob Davis <jacob.marquez.davis@hotmail.com>
2020-09-28 16:00:42 -0400

    Added insert script for activity

---
+++ Week 2/activity.sql
@@ -0,0 +1,20 @@
+INSERT INTO apartments VALUES
+  (1, 'A', 1, 1250),
+  (2, 'B', 1, 1300),
+  (3, 'C', 1, 1500),
+  (4, 'C', 2, 1400);
+
+INSERT INTO residents VALUES
+  (1, 'Jacob', 'Davis', 1),
+  (2, 'Sally', 'Bobson', 2),
+  (3, 'Ricky', 'Bobson', 2),
+  (4, 'Martha', 'Stuart', 3),
+  (5, 'Jackie', 'Samson', 4);
+
+INSERT INTO cars VALUES
+  (1, 'Toyota', 'Corolla', 1995, 'IGB18SS', 2),
+  (2, 'Honda', 'Civic', 2001, 'FFGB91S', 3);
+
+INSERT INTO pets VALUES
+  (1, 'cat', 'Jimmy', 3, 0),
+  (2, 'dog', 'Spot', 4, 1);
