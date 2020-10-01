8dc78edebb60cf3588146bce64369fd58e06ced2
Jacob Davis <jacob.marquez.davis@hotmail.com>
2020-10-01 11:21:14 -0400

    Added preared statement example

--- Week 2/MyFirstJDBC/src/main/java/com/revature/App.java
+++ Week 2/MyFirstJDBC/src/main/java/com/revature/App.java
@@ -2,6 +2,7 @@ package com.revature;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -23,26 +24,13 @@ public class App {
 			// Its where Statement and PreparedStatement objects come from
 			connection = DriverManager.getConnection(connectionString, username, password);
 			
-			// The Statement object doesn't represent a statement; it's the vehicle for running statements
-			Statement stmt = connection.createStatement();
+			regularStatement(connection);
 			
-			// We use our Statement to execute a query, which will return a ResultSet
-			// ResultSets contain the rows returned by the query
-			ResultSet rs = stmt.executeQuery("select * from residents;");
+			preparedStatement(connection);
 			
-			// ResultSet has an internal iterator that starts at the row above the first row.
-			// We move it along by calling ResultSet.next().
-			// If there is no next row, next() returns false.
-			while (rs.next()) {
-				// In order to get data out of the row, we can call a variety of get methods, such as
-				// ResultSet.getString, and give either the column number or name as the parameter
-				// to identify the column.
-				System.out.println(rs.getString(1) + " " + rs.getString("last_name"));
-			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			
 			// We should close our connection at the end.
 			try {
 				if (connection != null)
@@ -51,6 +39,41 @@ public class App {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	public static void regularStatement(Connection connection) throws SQLException {
+		// The Statement object doesn't represent a statement; it's the vehicle for running statements
+		Statement stmt = connection.createStatement();
+		
+		// We use our Statement to execute a query, which will return a ResultSet
+		// ResultSets contain the rows returned by the query
+		ResultSet rs = stmt.executeQuery("select * from residents;");
+		
+		// ResultSet has an internal iterator that starts at the row above the first row.
+		// We move it along by calling ResultSet.next().
+		// If there is no next row, next() returns false.
+		while (rs.next()) {
+			// In order to get data out of the row, we can call a variety of get methods, such as
+			// ResultSet.getString, and give either the column number or name as the parameter
+			// to identify the column.
+			System.out.println(rs.getString(1) + " " + rs.getString("last_name"));
+		}
+	}
+	
+	public static void preparedStatement(Connection connection) throws SQLException {
+		// PreparedStatements are Statements that have question marks in them. The ?s represent
+		// parameters, or variables, that are then set after the PreparedStatement is created.
+		// The ?s can be anything.
+		PreparedStatement ps = connection.prepareStatement("select * from residents where id = ?");
+		
+		// We use PreparedStatement.set<Type>(position, value) to set the value of a particular parameter.
+		// As with columns in ResultSet.gets, the first ? is number 1.
+		ps.setInt(1, 2);
+		
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			System.out.println(rs.getString("first_name") + " " + rs.getString("last_name"));
 		}
 	}
 }
