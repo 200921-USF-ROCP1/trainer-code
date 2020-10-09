2def8f2a535cf5002ce9f865c477dd0c510520f9
Jacob Davis <jacob.marquez.davis@hotmail.com>
2020-10-09 11:28:23 -0400

    Error handling exercise done

--- Week 3/ErrorHandling/src/main/java/com/revature/servlets/ErrorServlet.java
+++ Week 3/ErrorHandling/src/main/java/com/revature/servlets/ErrorServlet.java
@@ -14,6 +14,32 @@ public class ErrorServlet extends HttpServlet {
 	      response.setContentType("text/html");
 	      
 	      // Do error stuff here!
+	      int statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+	      String exceptionType = ((Class) request.getAttribute("javax.servlet.error.exception_type")).toString();
+	      Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+	      String requestUri = (String) request.getAttribute("javax.servlet.error.request_uri");
+	      
+	      PrintWriter pw = response.getWriter();
+	      
+	      pw.print("<html><body>");
+	      
+	      if (exception != null) {
+	    	  pw.println("<p>Exception type: " + exceptionType + "</p>");
+	    	  pw.println("<p>Exception message: " + exception.getMessage() + "</p>");
+	    	  
+	    	  pw.println("<div>Exception stack trace: <ul>");
+	    	  
+	    	  StackTraceElement[] stackTrace = exception.getStackTrace();
+	    	  for (int i = 0; i < stackTrace.length; ++i) {
+	    		  pw.print("<li>" + stackTrace[i] + "</li>");
+	    	  }
+	    	  pw.print("</ul> </div>");
+	      }
+	      
+	      pw.print("<p>Status code: " + statusCode + "</p>");
+	      pw.print("<p>Request URI: " + requestUri + "</p>");
+	      
+	      pw.print("</body></html>");
 	}
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
